817f432d2e3edb8dd64514586896b31db8568ea5
Julien Verneaut <jverneaut@gmail.com>
2020-05-28 10:57:12 +0200

    Improve console output

--- packages/lib/src/utils/log.js
+++ packages/lib/src/utils/log.js
@@ -5,11 +5,14 @@ export const logVersion = () => {
   const fgColor = '#ffffff';
   const bgColor = '#0000ff';
 
-  console.log(`%c *** gl v${version} ***`, `background: ${bgColor}; color: ${fgColor};`);
+  console.log(
+    `%c - gl v${version} - `,
+    `background: ${bgColor}; color: ${fgColor}; font-weight: bold;`
+  );
 };
 
 export const log = (...args) => {
   const date = new Date().toLocaleTimeString();
 
-  console.log.apply(console, [`gl [${date}] -`, ...args]);
+  console.log.apply(console, [`%c gl [${date}]`, 'font-weight: bold;', ...args]);
 };
